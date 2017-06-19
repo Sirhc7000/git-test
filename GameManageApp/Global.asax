ae963931fed265bbe3b2e222299296f965f61ee9
Chris J <sirhc7000@gmail.com>
2017-06-19 16:19:18 -0700

    Initial Commit

---
+++ GameManageApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameManageApp.MvcApplication" Language="C#" %>
---
+++ GameManageApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameManageApp.Startup))]
+namespace GameManageApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
